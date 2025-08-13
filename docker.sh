3441b6e145594795de4a06567b88aa243d18556a
RAVIRAVEEN <raviraveen7@gmail.com>
2025-08-13 21:40:43 +0530

    dockerfile

--- docker.sh
+++ docker.sh
@@ -58,8 +58,8 @@ VALIDATE $? "added ec2-user  to docker group"
 
 echo -e "$R Logout and login again $N"
 
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
-  
+curl -LO https://dl.k8s.io/release/v1.33.0/bin/linux/amd64/kubectl
+
 chmod +x kubectl
 
 mkdir -p ~/.local/bin
